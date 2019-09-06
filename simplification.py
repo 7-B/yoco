a70f5376d1045b123d47b03b3fac3a5432a78650
June <rlawns3244@naver.com>
2019-09-06 12:29:20 +0900

    segmentation done

--- simplification.py
+++ simplification.py
@@ -5,7 +5,11 @@ from torch.utils.serialization import load_lua
 
 from PIL import Image
 
-def simplify():
+# Check prcess time
+import time 
+def simplify(path):
+   t0 = time.time()
+
    use_cuda = torch.cuda.device_count() > 0
 
    cache  = load_lua('model_gan.t7')
@@ -14,7 +18,7 @@ def simplify():
    imstd  = cache.std
    model.evaluate()
 
-   data  = Image.open('sketchKeras.jpg').convert('L')
+   data  = Image.open(path).convert('L')
    w, h  = data.size[0], data.size[1]
    pw    = 8-(w%8) if w%8!=0 else 0
    ph    = 8-(h%8) if h%8!=0 else 0
@@ -23,7 +27,13 @@ def simplify():
       data = torch.nn.ReplicationPad2d( (0,pw,0,ph) )( data ).data
 
    if use_cuda:
+      print("PyTorch is using CUDA")
+      print("CUDA device count :",torch.cuda.device_count())
+      print("GPU :",torch.cuda.get_device_name(0))
       pred = model.cuda().forward( data.cuda() ).float()
    else:
       pred = model.forward(data)
    save_image( pred[0], 'out.png')
+   t1 = time.time()
+   total = t1-t0
+   print(total,"sec spent")
